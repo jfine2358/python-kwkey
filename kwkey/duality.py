5571b0f855afb9e4c83f47586303f491647283bf
Jonathan Fine <jfine2358@gmail.com>
2020-09-09 16:28:22 +0100

    Add duality.getattr_loop(obj, names).

--- kwkey/duality.py
+++ kwkey/duality.py
@@ -11,3 +11,17 @@ def make_name_dict(char):
         'set': (f'_{char}_set__', '__set__'),
         'del': (f'_{char}_del__', '__del__'),
     }
+
+
+def getattr_loop(obj, names):
+
+    sentinel = object()
+
+    for name in names:
+
+        value = getattr(obj, name, sentinel)
+        if value is not sentinel:
+            return value
+
+    # Redo without default to raise exception.
+    getattr(obj, name)
--- kwkey/test_duality.py
+++ kwkey/test_duality.py
@@ -1,6 +1,4 @@
-'''
-
->>> name_dict = make_name_dict('A')
+'''>>> name_dict = make_name_dict('A')
 
 >>> name_dict['get']
 ('_A_get__', '__get__')
@@ -11,6 +9,35 @@
 >>> name_dict['del']
 ('_A_del__', '__del__')
 
+
+
+Test data for getattr_loop.
+>>> class X: pass
+>>> x = X(); x.aaa = 1; x.bbb = 2
+
+Always, return first attribute found, else raise AttributeError.
+
+>>> getattr_loop(x, ('aaa',))
+1
+>>> getattr_loop(x, ('bbb',))
+2
+>>> getattr_loop(x, ('ccc',))
+Traceback (most recent call last):
+AttributeError: 'X' object has no attribute 'ccc'
+
+>>> getattr_loop(x, ('aaa', 'bbb'))
+1
+
+>>> getattr_loop(x, ('bbb', 'aaa'))
+2
+
+>>> getattr_loop(x, ('aaa', 'ccc'))
+1
+
+>>> getattr_loop(x, ('ccc', 'aaa'))
+1
+
 '''
 
 from .duality import make_name_dict
+from .duality import getattr_loop
