e5910cb8ad6a4e61fa0a05465f09a055742a93cb
Jonathan Fine <jfine2358@gmail.com>
2020-09-09 15:58:30 +0100

    Add duality.make_name_dict(char).

--- kwkey/duality.py
+++ kwkey/duality.py
@@ -2,3 +2,12 @@
 >>> 1 + 1
 2
 '''
+
+
+def make_name_dict(char):
+
+    return {
+        'get': (f'_{char}_get__', '__get__'),
+        'set': (f'_{char}_set__', '__set__'),
+        'del': (f'_{char}_del__', '__del__'),
+    }
--- kwkey/test_duality.py
+++ kwkey/test_duality.py
@@ -1,1 +1,16 @@
-1 + 2
+'''
+
+>>> name_dict = make_name_dict('A')
+
+>>> name_dict['get']
+('_A_get__', '__get__')
+
+>>> name_dict['set']
+('_A_set__', '__set__')
+
+>>> name_dict['del']
+('_A_del__', '__del__')
+
+'''
+
+from .duality import make_name_dict
